6531acf3e356a2e343798f2996a32fef3c6cae34
Brian Harvey <brian.harv3y@gmail.com>
2020-08-23 21:24:34 -0700

    fix typo

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+//Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = [];
 }
 
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+//Business Logic for Contacts------------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
