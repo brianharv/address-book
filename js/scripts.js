5a4de87a1a0ea49c13511e248349a37e7d1efeaa
Brian Harvey <brian.harv3y@gmail.com>
2020-08-23 21:52:57 -0700

    add findcontact and deletecontact to addressBook

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,27 @@ AddressBook.prototype.assignId = function() {
   return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i<this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      return this.contacts[i];
+    }
+  };
+  return false; //this is the break once it has looped through all ID's once (otherwise it's infinitely looping)
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }  
+    }
+  };
+  return false;
+}
+
 //Business Logic for Contacts------------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
