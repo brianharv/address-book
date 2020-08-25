e1e586c86352afdbff5b86e9ee0b7517d6ce7129
Brian Harvey <brian.harv3y@gmail.com>
2020-08-24 20:36:19 -0700

    add new html, css, and scripts to complete event bubbling and event delegation lession.

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,55 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+//User Interface Logic
+let addressBook = new AddressBook(); //THIS IS A GLOBAL VARIABLE AND WE DONT WANT THESE IN THE REAL WORLD
+
+function displayContactDetails(addressBookToDisplay) {  //This is subbosed to be outside of the submission listener. It simplifies things.
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  addressBookToDisplay.contacts.forEach(function(contact) {
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button=");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function () {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    attachContactListeners();
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#phone-number").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+});
